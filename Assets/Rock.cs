c9d5ff863e9d56d76aafd0ba2c3e82c337144744
상빈 <67950060+17642@users.noreply.github.com>
2024-01-01 17:12:36 +0900

    Part 12

--- Assets/Rock.cs
+++ Assets/Rock.cs
@@ -20,19 +20,25 @@ public class Rock : MonoBehaviour
     [SerializeField]
     private GameObject go_effect_prefabs;//이펙트 프리팹
 
-    //임시 오디오
+    /*//임시 오디오
     [SerializeField]
     private AudioSource audioSource;
     [SerializeField]
     private AudioClip effect_sound;
     [SerializeField]
     private AudioClip effect_sound2;
+    */
+    [SerializeField]
+    private string strike_sound;
+    [SerializeField]
+    private string destroy_sound;
 
 
     public void Mining()
     {
-        audioSource.clip = effect_sound;
-        audioSource.Play();
+        SoundManager.instance.PlaySE(strike_sound);
+        //audioSource.clip = effect_sound;
+        //audioSource.Play();
         var clone=  Instantiate(go_effect_prefabs,coll.bounds.center,Quaternion.identity);//이펙트 물체를 바위에 생성
         Destroy(clone, delayTime);//이펙트 지속 시간 후 파괴
         hp--;
@@ -44,8 +50,9 @@ public class Rock : MonoBehaviour
 
     private void Destruction()
     {
-        audioSource.clip = effect_sound2;
-        audioSource.Play();
+        //audioSource.clip = effect_sound2;
+        //audioSource.Play();
+        SoundManager.instance.PlaySE(destroy_sound);
         coll.enabled = false;//콜라이더 비활성화
         Destroy(go_rock);//일반 바위 파괴
         go_debris.SetActive(true);//파편 활성화
---
+++ Assets/SoundManager.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[System.Serializable]//클래스를 직렬화
+public class Sound//이 클래스는 컴포넌트로 사용할 수 없음.
+{
+    public string name;
+    public AudioClip clip;
+}
+
+public class SoundManager : MonoBehaviour
+{
+    #region Singleton
+    //싱글톤화 (일반적으로 1개만 사용)
+    static public SoundManager instance;
+    private void Awake()
+    {
+        if(instance == null)//instance에 아무것도 없을 경우
+        {
+            instance = this;//자기 자신 삽입
+            DontDestroyOnLoad(gameObject);//씬 전환 시 파괴 방지
+        }
+        else
+        {
+            Destroy(this.gameObject);//다른 무언가가 들어와 있을 경우 파괴
+        }
+    }
+    #endregion Singleton
+    public AudioSource[] audioSourceEffects;
+    public AudioSource audioSourceBgm;
+
+    [SerializeField]
+    public Sound[] effectSounds;
+    [SerializeField]
+    public Sound[] bgmSounds;
+
+    public string[] playSoundName;
+
+    private void Start()
+    {
+        playSoundName = new string[audioSourceEffects.Length];//소리 이름 배열을 오디오소스 수와 일치
+    }
+
+    public void PlaySE(string _name)
+    {
+        for(int i = 0; i < effectSounds.Length; i++)
+        {
+            if (_name == effectSounds[i].name)//맞는 이름의 SoundEffect 탐색
+            {
+                for(int j=0; j < audioSourceEffects.Length; j++)//빈 AudioSource 탐색
+                {
+                    if (!audioSourceEffects[j].isPlaying)
+                    {
+                        playSoundName[j] = effectSounds[i].name;//이름 지정
+                        audioSourceEffects[i].clip = effectSounds[j].clip;
+                        audioSourceEffects[i].Play();
+                        return;
+                    }
+                }
+                Debug.Log("모든 AUDIOSOURCE 사용중");
+                return;
+            }
+            Debug.Log(_name + " 사운드가 등록되지 않았습니다.");
+        }
+        
+    }
+
+    public void StopAllSE()
+    {
+        for(int i = 0; i < audioSourceEffects.Length; i++)
+        {
+            audioSourceEffects[i].Stop();//모든 AudioSource Stop;
+        }
+    }
+
+    public void StopSE(string _name)
+    {
+        for(int i=0;i<audioSourceEffects.Length; i++)
+        {
+            if (playSoundName[i] == _name)
+            {
+                audioSourceEffects[i].Stop();
+                break;
+            }
+        }
+        Debug.Log("재생 중인" + _name + "사운드가 없습니다.");
+    }
+
+}
