9fa95699a1ded09fa5b549bcf187f16287595dd1
상빈 <67950060+17642@users.noreply.github.com>
2024-01-14 09:42:11 +0900

    Part 29

---
+++ Assets/Scripts/DayAndNight.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayAndNight : MonoBehaviour
+{
+    [SerializeField]
+    private float secondPerRealTimeSecond;//현실과 게임상의 시간의 비
+    //Fog 밀고
+
+    [SerializeField]
+    private float fogDensityCalc;//안개 밀도 계산
+    [SerializeField]
+    private float nightFogDensity;
+    [SerializeField]
+    private float dayFogDensity;
+    private float currentFogDensity;//계산
+
+    private bool isNight = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right, 0.1f * secondPerRealTimeSecond * Time.deltaTime);
+        if (transform.eulerAngles.x >= 170)
+        {
+            isNight = true;//해의 각도가 일정 이상이면 night 활성화
+        }else if(transform.eulerAngles.x <= 340.0f)
+        {
+            isNight = false;
+        }
+
+        if (isNight)
+        {
+            if (currentFogDensity <= nightFogDensity)
+            {
+                currentFogDensity += 0.1f * fogDensityCalc * Time.deltaTime;
+                RenderSettings.fogDensity = currentFogDensity;//안개 밀도 설정
+            }
+        }
+        else
+        {
+            if (currentFogDensity >= dayFogDensity)
+            {
+                currentFogDensity -= 0.1f * fogDensityCalc * Time.deltaTime;
+                RenderSettings.fogDensity = currentFogDensity;//안개 밀도 설정
+            }
+        }
+    }
+}
